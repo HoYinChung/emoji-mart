156e0bed8cfa7e3a4b059d250d2c2740782638e5
sunzhangjie <sunzhangjie@formssi.com>
2019-03-22 03:07:44 +0800

    mobile emoji

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ export { default as frequently } from './utils/frequently'
 
 export { default as Picker } from './components/picker/picker'
 export { default as NimblePicker } from './components/picker/nimble-picker'
+export { default as HorizonPicker } from './components/picker/horizon-picker'
+export { default as HorizonNimblePicker } from './components/picker/horizon-nimble-picker'
 export { default as Emoji } from './components/emoji/emoji'
 export { default as NimbleEmoji } from './components/emoji/nimble-emoji'
 export { default as Category } from './components/category'
